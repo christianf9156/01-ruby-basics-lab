46704ed961437a4f78214d4e2eb0f6b4f7a05e54
christianf9156 <christianf9156@hstat.org>
2016-10-13 00:47:44 +0000

    finished HW

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,24 @@
-def division
-    
+def division(num_one,num_two)
+    num_one / num_two
 end
 
-def argue
-  
+def argue(word)
+  return word
 end
 
-def greeting
-
+def greeting(greeting,name)
+    puts "#{greeting} #{name}, how are you doing this fine day?"
 end
 
-def return_a_value
-    
+def return_a_value()
+   
+    return "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(topping="cheese")
+    "#{topping}"
 end
 
-def whisper
-    
+def whisper(silent_word)
+    "#{silent_word.downcase}"
 end
